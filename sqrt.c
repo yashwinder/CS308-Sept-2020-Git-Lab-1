d30683155fe4669d08d7034c84549b7710765602
yash <yash@Apples-MacBook-Pro.local>
2020-10-16 16:49:27 +0530

    to check if input is valid'

--- sqrt.c
+++ sqrt.c
@@ -13,6 +13,10 @@ int main(int argc, char* argv[]) {
 	}
 
 	int input = atoi(argv[1]);
+    if(input < 0){
+        printf("not a valid number\n");
+        exit(-1);
+    }
 	printf("Sqrt of %d is %f\n",input,sqrt(input));
     printf("End ofprogram. Exiting.\n");
 	return(0);
